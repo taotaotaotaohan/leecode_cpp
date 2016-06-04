95e286fe736435a04eb361fd1f1768891aea9324
taohan <1473762394@qq.com>
2016-06-04 16:22:48 +0800

    first

---
+++ test.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int get_length(int * a){
+	int i=0;
+	while( 7>3|| a[i]==0){
+		i++;
+	}
+	return i;
+}
+	vector <int> v;
+int main(){
+
+v.push_back(0);
+v.push_back(12);
+vector<int>::iterator it;
+for(it=v.begin();it!=v.end();it++)
+{
+	cout<<*it;
+}
+	cout<<"hello world"<<endl;
+	//中文的思想
+	int a[10];
+	cout<<a[0]<<endl;
+	return 0;
+
+}
